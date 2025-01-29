aca2883abae78908dff90ea5f53849ad4f01bc4a
aniketamte <aniketamte123@gmail.com>
2025-01-29 22:00:06 +0530

    Leetcode 2976

---
+++ Microsoft/Leetcode2976.java
@@ -0,0 +1,12 @@
+public class Leetcode2976 {
+      //6Companies 30Days Challenge
+      //2976. Minimum Cost to Convert String I
+
+      public static void main(String[] args) {
+            String source = "abcd";
+            String target = "acbe";
+            String original[] = {"a", "b", "c", "c", "e", "d"};
+            String changed[] = {"b", "c", "b", "e", "b", "e"};
+            int cost[] = {2, 5, 5, 1, 2, 20};
+      }
+}
