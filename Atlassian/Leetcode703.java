76da6e274f6981af87a7b8cf1164ce63a0beccb6
aniketamte <aniketamte123@gmail.com>
2025-01-29 21:50:24 +0530

    Leetcode Question 703

--- Atlassian/Leetcode703.java
+++ Atlassian/Leetcode703.java
@@ -3,6 +3,14 @@ public class Leetcode703 {
     //6Company 30 Days Challenge 
     //Leetcode 703 ==> Kth Largest Element in a Stream
 
+    public static KthLargest(int k, int[] nums) {
+
+    }
+
+    public static int add(int val) {
+
+    }
+
     public static void main(String[] args) {
 
     }
