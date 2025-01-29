9000f6972b2d26ab82f9f446e5d8c4c31614d3fa
aniketamte <aniketamte123@gmail.com>
2025-01-29 21:53:13 +0530

    Leetcode Question 703

--- Atlassian/Leetcode703.java
+++ Atlassian/Leetcode703.java
@@ -1,10 +1,19 @@
 
+import java.util.PriorityQueue;
+
+
 public class Leetcode703 {
     //6Company 30 Days Challenge 
     //Leetcode 703 ==> Kth Largest Element in a Stream
 
+    PriorityQueue<Integer> pq;
+    int k;
     public static KthLargest(int k, int[] nums) {
-
+      this.k = k;
+      pq = new PriorityQueue<>();
+      for(int num : nums){
+            add(num);
+      }
     }
 
     public static int add(int val) {
